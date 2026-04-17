80e0961803a181faee9ca80deb1414d849cf6fd3
Kornnalin <firnfroy@gmail.com>
2026-04-17 17:53:42 +0700

    -- import api xbyk #1 -- updated import api xbyk updated Visibility of import api add try catch and logError of import content

--- Kentico.Xperience.KSP.Migrate/Services/ContentTypeImportService.cs
+++ Kentico.Xperience.KSP.Migrate/Services/ContentTypeImportService.cs
@@ -2,8 +2,10 @@
 using CMS.FormEngine;
 using Kentico.Xperience.KSP.Migrate.Models.API;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 
 namespace Kentico.Xperience.KSP.Migrate.Services
 {
@@ -11,83 +13,126 @@ namespace Kentico.Xperience.KSP.Migrate.Services
     {
         public (string message, string codeName, int fieldCount) Import(ContentTypeDto model)
         {
-            DataClassInfo contentType;
-            var formXmlChanged = false;
-            var msg = "";
-
-            var existing = DataClassInfoProvider.GetDataClassInfo(model.CodeName);
-            contentType = existing ?? new DataClassInfo
+            try 
             {
-                ClassName = model.CodeName,
-                ClassDisplayName = model.Name,
-                ClassTableName = model.CodeName.Replace(".", "_")
-            };
+                if (model == null)
+                    throw new Exception("Model is null");
 
-            var formXml = string.IsNullOrWhiteSpace(contentType.ClassFormDefinition)
-                            ? "<form></form>"
-                            : contentType.ClassFormDefinition;
+                DataClassInfo contentType;
+                var formXmlChanged = false;
+                var msg = "";
 
-            var formDefinition = new FormInfo(formXml);             
-
-            foreach (var f in model.Fields)
-            {
-                var field = formDefinition.GetFormField(f.Name);
-
-                var allowempty = f.IsRequired ? "false" : "true";
-                var sizeXml = (f.Size.HasValue && f.Size > 0) ? f.Size.Value : 200;
-                var caption = string.IsNullOrEmpty(f.Caption) ? f.Name : f.Caption;                 
-                var defaultXml = string.IsNullOrEmpty(f.DefaultValue) ? "" : $"<defaultvalue>{System.Security.SecurityElement.Escape(f.DefaultValue)}</defaultvalue>";
-
-                //var minItems = (f.MinItems.HasValue && f.MinItems > 0) ? f.MinItems.Value : 1;
-                //var maxItems = (f.MaxItems.HasValue && f.MaxItems > 0) ? f.MaxItems.Value : 1;
-
-                var minMaxXml = "";
-                if (f.MinItems.HasValue)
+                var existing = DataClassInfoProvider.GetDataClassInfo(model.CodeName);
+                contentType = existing ?? new DataClassInfo
                 {
-                    minMaxXml += $"<minitems>{f.MinItems.Value}</minitems>";
+                    ClassName = model.CodeName,
+                    ClassDisplayName = model.Name,
+                    ClassTableName = model.CodeName.Replace(".", "_"),
+
+                    ClassContactOverwriteEnabled = false,
+                    ClassCodeGenerationSettings = GenerateClassCodeGenerationSettings(model.CodeName),
+                    ClassIconClass = "icon-kentico",
+                    ClassHasUnmanagedDbSchema = false,
+                    ClassWebPageHasUrl = false, //0 = No URL, 1 = Have URL
+
+                    ClassType = "Content",
+                    ClassContentTypeType = "Website",
+                    ClassShortName = GenerateClassShortName(model.CodeName)
+                };
+
+                //var formXml = string.IsNullOrWhiteSpace(contentType.ClassFormDefinition)
+                //                ? "<form></form>"
+                //                : contentType.ClassFormDefinition;
+
+                var formXml = contentType.ClassFormDefinition;
+
+                if (string.IsNullOrWhiteSpace(formXml))
+                {
+                    //CREATE ใหม่ ต้องมี Primary Key
+                    //formXml = @"
+                    //    <form>
+                    //        <field column=""ContentItemDataID""
+                    //               columntype=""integer""
+                    //               ispk=""true""
+                    //               identity=""true""
+                    //               allowempty=""false""
+                    //               visible=""false"" />
+                    //    </form>";
+
+                    formXml = @"
+                            <form>
+                                <field column=""ContentItemDataID"" columntype=""integer"" ispk=""true"" enabled=""true"" />
+                                <field column=""ContentItemDataCommonDataID"" columntype=""integer"" system=""true"" />
+                                <field column=""ContentItemDataGUID"" columntype=""guid"" system=""true"" isunique=""true"" />
+                            </form>";
                 }
 
-                if (f.MaxItems.HasValue)
+                var formDefinition = new FormInfo(formXml);
+
+                //Process fields : Create new or update existing
+                foreach (var f in model.Fields)
                 {
-                    minMaxXml += $"<maxitems>{f.MaxItems.Value}</maxitems>";
-                }
-
-                var dropdownOptions = "";
-                if (f.FieldType?.ToLower() == "dropdown")
-                {
-                    dropdownOptions = BuildDropdownOptions(f.DataSource);
-                }
-
-                var controlName = MapFormControl(f.FieldType, f.DataType);
-
-                var allowedTypesXml = "";
-
-                if (f.FieldType?.ToLower() == "contentitemselector" && f.AllowedContentTypes?.Any() == true)
-                {
-                    var guids = new List<string>();
-
-                    foreach (var codeName in f.AllowedContentTypes)
+                    try
                     {
-                        var classInfo = DataClassInfoProvider.GetDataClassInfo(codeName);
-                        if (classInfo != null)
+                        var field = formDefinition.GetFormField(f.Name);
+
+                        var allowempty = f.IsRequired ? "false" : "true";
+                        var sizeXml = (f.Size.HasValue && f.Size > 0) ? f.Size.Value : 200;
+                        var caption = string.IsNullOrEmpty(f.Caption) ? f.Name : f.Caption;
+                        var defaultXml = string.IsNullOrEmpty(f.DefaultValue) ? "" : $"<defaultvalue>{System.Security.SecurityElement.Escape(f.DefaultValue)}</defaultvalue>";
+
+                        //var minItems = (f.MinItems.HasValue && f.MinItems > 0) ? f.MinItems.Value : 1;
+                        //var maxItems = (f.MaxItems.HasValue && f.MaxItems > 0) ? f.MaxItems.Value : 1;
+
+                        var minMaxXml = "";
+                        if (f.MinItems.HasValue)
                         {
-                            guids.Add(classInfo.ClassGUID.ToString());
+                            minMaxXml += $"<minitems>{f.MinItems.Value}</minitems>";
                         }
-                    }
 
-                    if (guids.Any())
-                    {
-                        var json = System.Text.Json.JsonSerializer.Serialize(guids);
-                        allowedTypesXml = $"<AllowedContentItemTypeIdentifiers>{json}</AllowedContentItemTypeIdentifiers>";
-                    }
-                }
+                        if (f.MaxItems.HasValue)
+                        {
+                            minMaxXml += $"<maxitems>{f.MaxItems.Value}</maxitems>";
+                        }
 
-                var visible = f.Visible;
+                        var dropdownOptions = "";
+                        if (f.FieldType?.ToLower() == "dropdown")
+                        {
+                            dropdownOptions = BuildDropdownOptions(f.DataSource);
+                        }
 
-                if (field == null)
-                {
-                    //Create
-                    var fieldXml = $@"
+                        var controlName = MapFormControl(f.FieldType, f.DataType);
+
+                        var allowedTypesXml = "";
+
+                        if (f.FieldType?.ToLower() == "contentitemselector" && f.AllowedContentTypes?.Any() == true)
+                        {
+                            var guids = new List<string>();
+
+                            foreach (var codeName in f.AllowedContentTypes)
+                            {
+                                var classInfo = DataClassInfoProvider.GetDataClassInfo(codeName);
+                                if (classInfo != null)
+                                {
+                                    guids.Add(classInfo.ClassGUID.ToString());
+                                }
+                            }
+
+                            if (guids.Any())
+                            {
+                                var json = System.Text.Json.JsonSerializer.Serialize(guids);
+                                allowedTypesXml = $"<AllowedContentItemTypeIdentifiers>{json}</AllowedContentItemTypeIdentifiers>";
+                            }
+                        }
+
+                        var visible = f.Visible;
+                        var visibilityXml = f.Visibility != null ? BuildVisibilityXml(f.Visibility) : "";
+
+
+                        if (field == null)
+                        {
+                            //Create
+                            var fieldXml = $@"
                                     <field column=""{f.Name}""
                                            columntype=""{MapToKenticoType(f.DataType, f.FieldType)}""
                                            columnsize=""{sizeXml}""
@@ -103,74 +148,90 @@ namespace Kentico.Xperience.KSP.Migrate.Services
                                             {dropdownOptions}
                                             {minMaxXml}
                                         </settings>
+                                        {visibilityXml}
                                     </field>";
 
-                    formXml = formXml.Replace("</form>", fieldXml + "\n</form>");
-                    msg = "Created";
-                }
-                else
-                {
-                    //Update
-                    field.Caption = caption;
-                    field.AllowEmpty = allowempty == "true";
-                    field.Size = sizeXml;
-                    field.Settings["controlname"] = controlName;
-                    field.Visible = f.Visible;
-
-                    field.DataType = MapToKenticoType(f.DataType, f.FieldType);
-
-                    if (!string.IsNullOrEmpty(f.DefaultValue))
-                    {
-                        field.DefaultValue = f.DefaultValue;
-                    }
-
-                    if (f.FieldType?.ToLower() == "dropdown" && !string.IsNullOrEmpty(f.DataSource))
-                    {
-                        field.Settings["Options"] = f.DataSource;
-                    }
-
-                    if (f.FieldType?.ToLower() == "contentitemselector" && f.AllowedContentTypes?.Any() == true)
-                    {
-                        var guids = new List<string>();
-
-                        foreach (var codeName in f.AllowedContentTypes)
+                            formXml = formXml.Replace("</form>", fieldXml + "\n</form>");
+                            msg = "Created";
+                        }
+                        else
                         {
-                            var classInfo = DataClassInfoProvider.GetDataClassInfo(codeName);
-                            if (classInfo != null)
+                            //Update
+                            field.Caption = caption;
+                            field.AllowEmpty = allowempty == "true";
+                            field.Size = sizeXml;
+                            field.Settings["controlname"] = controlName;
+                            field.Visible = f.Visible;
+
+                            field.DataType = MapToKenticoType(f.DataType, f.FieldType);
+
+                            if (!string.IsNullOrEmpty(f.DefaultValue))
                             {
-                                guids.Add(classInfo.ClassGUID.ToString());
+                                field.DefaultValue = f.DefaultValue;
                             }
+
+                            if (f.FieldType?.ToLower() == "dropdown" && !string.IsNullOrEmpty(f.DataSource))
+                            {
+                                field.Settings["Options"] = f.DataSource;
+                            }
+
+                            if (f.FieldType?.ToLower() == "contentitemselector" && f.AllowedContentTypes?.Any() == true)
+                            {
+                                var guids = new List<string>();
+
+                                foreach (var codeName in f.AllowedContentTypes)
+                                {
+                                    var classInfo = DataClassInfoProvider.GetDataClassInfo(codeName);
+                                    if (classInfo != null)
+                                    {
+                                        guids.Add(classInfo.ClassGUID.ToString());
+                                    }
+                                }
+
+                                if (guids.Any())
+                                {
+                                    field.Settings["AllowedContentItemTypeIdentifiers"] =
+                                        System.Text.Json.JsonSerializer.Serialize(guids);
+                                }
+                            }
+
+                            if (f.MinItems.HasValue)
+                            {
+                                field.Settings["minitems"] = f.MinItems.Value.ToString();
+                            }
+
+                            if (f.MaxItems.HasValue)
+                            {
+                                field.Settings["maxitems"] = f.MaxItems.Value.ToString();
+                            }
+
+                            formXmlChanged = true;
+                            msg = "Updated";
+
+                            formDefinition = ApplyVisibility(formDefinition, f.Name, f.Visibility);
                         }
-
-                        if (guids.Any())
-                        {
-                            field.Settings["AllowedContentItemTypeIdentifiers"] =
-                                System.Text.Json.JsonSerializer.Serialize(guids);
-                        }
                     }
-
-                    if (f.MinItems.HasValue)
+                    catch (Exception exField)
                     {
-                        field.Settings["minitems"] = f.MinItems.Value.ToString();
+                        //log ราย field
+                        LogError($"[FIELD ERROR] {model.CodeName} - {f.Name}", exField);
                     }
 
-                    if (f.MaxItems.HasValue)
-                    {
-                        field.Settings["maxitems"] = f.MaxItems.Value.ToString();
-                    }
-
-                    formXmlChanged = true;
-                    msg = "Updated";
                 }
+
+                contentType.ClassFormDefinition = formXmlChanged
+                    ? formDefinition.GetXmlDefinition()
+                    : formXml;
+
+                DataClassInfoProvider.SetDataClassInfo(contentType);
+
+                return (msg, model.CodeName, model.Fields.Count);
             }
-
-            contentType.ClassFormDefinition = formXmlChanged
-                ? formDefinition.GetXmlDefinition()
-                : formXml;
-
-            DataClassInfoProvider.SetDataClassInfo(contentType);
-
-            return (msg, model.CodeName, model.Fields.Count);
+            catch (Exception ex)
+            {
+                LogError($"Failed to import content type '{model.CodeName}'", ex);
+                return ($"Error: {ex.Message}", model.CodeName, model.Fields?.Count ?? 0);
+            } 
         }
 
         private string MapToKenticoType(string type, string fieldType)
@@ -220,6 +281,101 @@ namespace Kentico.Xperience.KSP.Migrate.Services
 
             //return dataSource;
             return $"<options>{dataSource}</options>";
+        }
+
+        private string GenerateClassShortName(string codeName)
+        { 
+            var baseName = codeName.Replace(".", "");             
+            var randomPart = GenerateRandomString(8);             
+            return baseName + randomPart;
+        }
+
+        private static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        } 
+
+        public static string GenerateClassCodeGenerationSettings(string className)
+        {
+            var nameSpace = GetNamespaceFromClassName(className);
+
+            return $"<Data><NameSpace>{nameSpace}</NameSpace></Data>";
+        }
+        public static string GetNamespaceFromClassName(string className)
+        {
+            if (string.IsNullOrWhiteSpace(className))
+                return string.Empty;
+
+            var parts = className.Split('.');
+            return parts.Length > 0 ? parts[0] : string.Empty;
+        }
+
+        private string BuildVisibilityXml(VisibilityConditionDto v)
+        {
+            if (v == null) return "";
+
+            return $@"
+                    <visibilityconditiondata>
+                      <VisibilityConditionConfiguration>
+                        <Identifier>{v.Operator}</Identifier>
+                        <Properties>
+                          <PropertyName>{v.Field}</PropertyName>
+                          <Value>{v.Value}</Value>
+                          <CaseSensitive>{v.CaseSensitive.ToString().ToLower()}</CaseSensitive>
+                        </Properties>
+                      </VisibilityConditionConfiguration>
+                    </visibilityconditiondata>";
+        }
+
+        private FormInfo ApplyVisibility(FormInfo formDefinition, string fieldName, VisibilityConditionDto visibility)
+        {
+            if (visibility == null) return formDefinition;
+
+            var target = formDefinition.GetFormField(visibility.Field);
+            if (target == null) return formDefinition;
+
+            var formXml = formDefinition.GetXmlDefinition();
+
+            var doc = new XmlDocument();
+            doc.LoadXml(formXml);
+
+            var fieldNode = doc.SelectSingleNode($"//field[@column='{fieldName}']");
+            if (fieldNode == null) return formDefinition;
+
+            var oldNode = fieldNode.SelectSingleNode("visibilityconditiondata");
+            oldNode?.ParentNode.RemoveChild(oldNode);
+
+            var xml = BuildVisibilityXml(visibility);
+
+            if (!string.IsNullOrEmpty(xml))
+            {
+                var fragment = doc.CreateDocumentFragment();
+                fragment.InnerXml = xml;
+
+                fieldNode.AppendChild(fragment);
+            }
+
+            return new FormInfo(doc.OuterXml);
+        }
+
+        private void LogError(string message, Exception ex)
+        {
+            var date = DateTime.Now.ToString("yyyyMMdd");
+
+            var fileName = $"logs/import-error-{date}.txt";
+
+            var log = $@"
+[{DateTime.Now}]
+{message}
+ERROR: {ex.Message}
+STACK: {ex.StackTrace}
+--------------------------";
+
+            System.IO.File.AppendAllText(fileName, log);
         }
     }
 }
